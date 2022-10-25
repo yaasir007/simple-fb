980db1e6f6e37becc24e4a088c74b5580637388d
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-10-25 13:53:25 +0400

    added back link in show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Post.destroy_all
 
 Post.create(name: "Yaasir", address: "Vallee Pitot", description: "testing", price_per_night: 400, number_of_guests: 5)
+Post.create(name: "Caasir", address: "Vallee Pitot", description: "testing", price_per_night: 400, number_of_guests: 5)
